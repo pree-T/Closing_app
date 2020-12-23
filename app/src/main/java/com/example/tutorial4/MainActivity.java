eeace5f94780f31157f260d3b044b73129a3698c
pree-T <miss.preetisarkar0107@gmail.com>
2020-12-23 09:14:53 +0530

    Closing app

---
+++ app/src/main/java/com/example/tutorial4/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.tutorial4;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static Button button_sbm;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        onButtonClickListener();
+    }
+    public void onButtonClickListener() {
+        button_sbm = (Button) findViewById(R.id.bt);
+        button_sbm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+                a_builder.setMessage("Do you want to close this app???").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                AlertDialog alert = a_builder.create();
+                alert.setTitle("Alert!!!");
+                alert.show();
+            }
+        });
+    }
+}
